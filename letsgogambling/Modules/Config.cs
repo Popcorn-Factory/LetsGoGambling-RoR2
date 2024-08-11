bdfada85a103f3db6108eed8dccccc869f7250a1
Ethanol10 <35375774+Ethanol10@users.noreply.github.com>
2024-08-11 20:34:39 +1000

    Final touches

---
+++ letsgogambling/Modules/Config.cs
@@ -0,0 +1,51 @@
+﻿using BepInEx.Configuration;
+using RiskOfOptions.Options;
+using RiskOfOptions;
+using RiskOfOptions.OptionConfigs;
+
+namespace LetsGoGambling.Modules
+{
+    public static class Config
+    {
+        public static ConfigEntry<float> volumeSlider;
+
+        public static void ReadConfig()
+        {
+            volumeSlider = LetsGoGamblingPlugin.instance.Config.Bind<float>
+            (
+                new ConfigDefinition("01 - Volume", "Volume Slider"),
+                50f,
+                new ConfigDescription("Changes the volume on the SFX. In-game Volume SFX affects this too!")
+            );
+        }
+
+        public static void SetupRiskOfOptions()
+        {
+            //Risk of Options intialization
+            ModSettingsManager.AddOption(
+                new StepSliderOption(
+                    volumeSlider,
+                    new StepSliderConfig
+                    {
+                        min = 0f,
+                        max = 100f,
+                        increment = 1f
+                    }
+                ));
+
+        }
+
+        public static void OnChangeHooks() 
+        {
+            volumeSlider.SettingChanged += VolumeSlider_SettingChanged;
+        }
+
+        private static void VolumeSlider_SettingChanged(object sender, System.EventArgs e)
+        {
+            if (AkSoundEngine.IsInitialized())
+            {
+                AkSoundEngine.SetRTPCValue("Volume_Gambling_SFX", volumeSlider.Value);
+            }
+        }
+    }
+}
